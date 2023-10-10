af14164f02250dbc1a5eb8df1d631ff2f6193171
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-10-10 06:09:27 +0200

    fix path construction

--- src/graphql/getSchema.ts
+++ src/graphql/getSchema.ts
@@ -3,15 +3,14 @@ import { getClient } from '../state.js'
 import { buildClientSchema, getIntrospectionQuery, printSchema } from 'graphql'
 
 async function getSchema(resource: string | undefined): Promise<void> {
-  const resource_ = resource !== null ? `/${resource}` : ''
-  const path = `api/graphql${resource_}`
+  const path = `api/graphql/${resource ?? ''}`
   const client = getClient()
   const introspectionQuery = getIntrospectionQuery()
   const response = await client
     .post(path)
-    .ok((_) => true)
     .set('content-Type', 'application/json')
     .send(JSON.stringify({ query: introspectionQuery }))
+    .ok((_) => true)
   if (response.status !== 200) {
     console.error(response.body)
     process.exit(1)
