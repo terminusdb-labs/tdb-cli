a669cd61614ed4653e9edbd4fd7421ae66d9a031
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-11-14 17:01:36 +0100

    fix

--- src/config/endpoint/index.ts
+++ src/config/endpoint/index.ts
@@ -1,6 +1,6 @@
 import { Command } from '@commander-js/extra-typings'
 import list from './list.js'
-import add from './command.js'
+import add from './add.js'
 const command = new Command()
   .name('endpoint')
   .description('endpoint operations')
